37511952715044e41f9b04175b0754736c0e0399
netion112 <nikoman02sp@gmail.com>
2024-06-17 12:07:27 +0700

    Perubahan Aset Figma
    Aset Ilustrasi Menyusul

---
+++ Assets/Scripts/quiz.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class quiz : MonoBehaviour
+{
+
+    public void question(bool answer)
+    {
+        int score;
+        if (answer)
+        {
+            score = PlayerPrefs.GetInt("score") + 10;
+        }
+        else
+        {
+            score = PlayerPrefs.GetInt("score") - 10;
+            if (score < 0)
+            {
+                score = 0;
+            }
+        }
+        PlayerPrefs.SetInt("score", score);
+
+        gameObject.SetActive(false);
+        transform.parent.GetChild(gameObject.transform.GetSiblingIndex()+1).gameObject.SetActive(true);
+    }
+}
