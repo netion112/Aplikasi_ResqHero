5246e5cc4d78f30c2bd9c21b759773f0ab8aa914
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2024-06-20 01:31:52 +0700

    feat: membuat splashscreen

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
+
+    public string sceneName;
+    public PlayableDirector playableDirector;
     
     public void SceneManagemen(string loadScene)
     {
@@ -14,12 +18,36 @@ public class LoadScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (playableDirector != null)
+        {
+            playableDirector.stopped += OnPlayableDirectorStopped;
+        }
+        else
+        {
+            Debug.LogWarning("PlayableDirector not assigned.");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    void OnPlayableDirectorStopped(PlayableDirector director)
+    {
+        if (director == playableDirector)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    void OnDestroy()
+    {
+        // Menghapus listener ketika objek dihancurkan untuk menghindari memory leak
+        if (playableDirector != null)
+        {
+            playableDirector.stopped -= OnPlayableDirectorStopped;
+        }
     }
 }
